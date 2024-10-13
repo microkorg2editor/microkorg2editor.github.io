6ed64a93366fbfdab1bc0d1e06deebb1361715a7
davis <itsmeeugarps@gmail.com>
2024-10-13 18:53:55 +0900

    Test

--- app.js
+++ app.js
@@ -157,8 +157,8 @@ function createTable(data)
 
             var slider = document.createElement("input");
             slider.type = 'range';
-            slider.min = 0;
-            slider.max = 127;
+            slider.min = nrpnParameters.knobMin;
+            slider.max = nrpnParameters.knobMax;
             slider.id = i;
             slider.oninput = function() 
             {
