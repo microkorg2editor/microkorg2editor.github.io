ff97806c4d1df59f4f801e7e3bde7818299d3054
davis <itsmeeugarps@gmail.com>
2024-10-15 20:23:49 +0900

    Add stub for receiving MIDI from synth, commented code for opening file from user's machine, make parameter table searchable and add value label

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ function startListening()
 {
     for (const input of midiIn) 
     {
-        // input.addEventListener('midimessage', midiMessageReceived);
+        input.addEventListener('midimessage', midiMessageReceived);
     }
 }
 
@@ -93,6 +93,27 @@ function sendMidiCC(Channel, CCNumber, value)
     if(device)
     {
         device.send(msg); 
+    }
+}
+
+function midiMessageReceived(event)
+{    
+    const statusByte = event.data[0];
+    const dataByte1 = event.data[1];
+    const dataByte2 = (event.data.length > 2) ? event.data[2] : 1;
+
+    if(statusByte == 0xF8) return;
+
+    switch(statusByte & 0xF0)
+    {
+        // CC
+        case 0xB0:
+        {
+            break;
+        }
+        
+        default:
+            break;
     }
 }
 
@@ -130,7 +151,7 @@ function programChangeSliderChange(channel, value)
 function createTable(data) 
 {
     const body = document.body, 
-    tbl = document.createElement('table');
+    tbl = document.getElementById('parameterTable');
     tbl.style.width = body.width;
   
     if(data)
@@ -153,8 +174,16 @@ function createTable(data)
           {
             // TODO : channel support
             sliderChange(0x0, mParameterData[this.id].CC, this.value);
+            var table = document.getElementById('parameterTable');
+            var element = table.getElementsByTagName("tr");
+            var td = element[this.id].getElementsByTagName("td");
+            td[2].innerHTML = this.value;
           }
-          cell.appendChild(slider);
+          const sliderCell = row.insertCell();
+          sliderCell.appendChild(slider);
+
+          const valueLabelCell = row.insertCell();
+          valueLabelCell.innerHTML = slider.value;
         }
 
         // NRPN params need special handling
@@ -201,6 +230,34 @@ function createTable(data)
     }
 }
 
+function searchParameterTable() 
+{
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("parameterSearchInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("parameterTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) 
+    {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) 
+      {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) 
+        {
+          tr[i].style.display = "";
+        } 
+        else 
+        {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+
 function loadJSON(file)
 {
     var rawFile = new XMLHttpRequest();
@@ -215,3 +272,35 @@ function loadJSON(file)
     }
     rawFile.send(null);
 }
+
+// // Select your input type file and store it in a variable
+// const input = document.getElementById('fileinput');
+
+// // This will upload the file after having read it
+// const upload = (file) => 
+// {
+//   fetch('http://www.example.net', 
+//   { // Your POST endpoint
+//     method: 'POST',
+//     headers:
+//     {
+//       // Content-Type may need to be completely **omitted**
+//       // or you may need something
+//       "Content-Type": "You will perhaps need to define a content-type here"
+//     },
+//     body: file // This is your file object
+//   }).then(
+//     response => response.json() // if the response is a JSON object
+//   ).then(
+//     success => console.log(success) // Handle the success response object
+//   ).catch(
+//     error => console.log(error) // Handle the error response object
+//   );
+// };
+
+// // Event handler executed when a file is selected
+// const onSelectFile = () => upload(input.files[0]);
+
+// // Add a listener on your input
+// // It will be triggered when a file will be selected
+// input.addEventListener('change', onSelectFile, false);
